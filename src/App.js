a10bbebe2b708516c1d3b99e8ffc7d3d25428d43
luke2k04 <luke2k04@gmail.com>
2021-03-30 00:34:12 +0200

    :poop: Added fetch to open Gdansk data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [stops, setStops] = useState();
+
+  useEffect(() => {
+    fetch("https://cors-anywhere.herokuapp.com/https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json")
+    .then(result => result.json())
+    .then(result => setStops(result))}, []);
+
+  if (!stops) {
+    return <>loading</>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          uwu
+          <input type="text" onChange={e => setSearchQuery(e.target.value)} />
         </p>
       </header>
     </div>
