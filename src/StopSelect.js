f68cbb4c83eaa386974c388a7e5d5812f6689a61
luke2k04 <luke2k04@gmail.com>
2021-04-03 17:33:11 +0200

    :construction: Added event handers for buttons.

--- src/StopSelect.js
+++ src/StopSelect.js
@@ -10,22 +10,25 @@ function StopSelect(props) {
             for (let i = 0; i < props.stops.length; i++) {
                 const req = await fetch(`https://ckan2.multimediagdansk.pl/delays?stopId=${props.stops[i]}`);
                 const data = await req.json();
-                headsigns.push(data.delay.map(item => item.headsign));
+                headsigns.push([data.delay.map(item => item.headsign), props.stops[i]]);
             }
-            headsigns = headsigns.map(arr => arr.reduce((acc, item) => {
+            headsigns = headsigns.map(arr => [arr[0].reduce((acc, item) => {
                 const prevOcc = acc.find(element => element === item)
                 if(!prevOcc) {
                     acc.push(item)
                 }
                 return acc
-            }, []))
+            }, []), arr[1]])
             setDelays(headsigns);
         })()
     }, [props])
 
     if(delays && delays.length) {
-        const headsignsTexts = delays.map(item => item.join(", "))
-        return headsignsTexts.map(item => <button>{item}</button>)
+        return delays.map(item => {
+            if(item[0] != "") {
+                return <button onClick={() => console.log(item[1])}>{item[0].join(", ")}</button>
+            }
+        })
     }
 
     return null
